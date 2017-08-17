a13c9cf44c53d8376e0a21fcbcad7151f6b0aca0
Danny Vasquez <dani22_vr@hotmail.com>
2017-08-17 17:46:56 -0500

    Eloquent create and update

--- ORM/app/Http/routes.php
+++ ORM/app/Http/routes.php
@@ -10,7 +10,29 @@
 | and give it the controller to call when that URI is requested.
 |
 */
+use App\User;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/create', function () {
+    /*return view('welcome');*/
+    /*return 'Archivo de home';*/
+    $user = User::create([
+    	'name' => 'Danny Vasquez',
+    	'email' => 'dani22_vr@hotmail.com',
+    	'password' => bcrypt('1234'),
+    	'gender' => 'M',
+    	'biography' => 'Curso eloquen basico'
+    ]);
+
+    return 'Usuario guardado';
+});
+
+Route::get('/update', function () {
+    $user = User::find(1);
+
+    $user->gender = 'M';
+    $user->biography = 'Curso eloquent basico';
+
+    $user->save();
+
+    return 'Usuario actualizado';
 });
