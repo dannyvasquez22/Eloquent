063f95aa1ca1f88dd78ac1c87aecc7cb8ee4ab7e
Danny Vasquez <dani22_vr@hotmail.com>
2017-08-19 02:17:52 -0500

    Methods OrderBy Take Get

--- ORM/resources/views/query/methods.blade.php
+++ ORM/resources/views/query/methods.blade.php
@@ -1,22 +1,22 @@
 @extends('layout')
 
 @section('content')
-	<div class="container">
-		<h1 class="page-header">{{ $title }}</h1>
-		
-		<table class="table table-hover table-striped">
-			<thead>
-				<tr>
-					<th>ID</th>
-					<th>Nombre</th>
-					<th>Email</th>
-					<th>Genero</th>
-					<th>Bibiografia</th>
-				</tr>
-			</thead>
-			<tbody>
-				@include('query.partials.list-users')
-			</tbody>
-		</table>		
-	</div>
+<div class="container">
+	<h1 class="page-header">{{ $title }}</h1>
+	
+	<table class="table table-hover table-striped">
+		<thead>
+			<tr>
+				<th>ID</th>
+				<th>Nombre</th>
+				<th>Email</th>
+				<th>Genero</th>
+				<th>Bibiografia</th>
+			</tr>
+		</thead>
+		<tbody>
+			@include('partials.list-users')
+		</tbody>
+	</table>		
+</div>
 @stop
